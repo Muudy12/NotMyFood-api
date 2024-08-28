699f6de56e88a4b0b82a9707de371fca430a6fd1
Mu Dah <dahmu24@gmail.com>
2024-08-28 12:02:50 -0600

    Added delete route for comments

--- Router/foods.js
+++ Router/foods.js
@@ -5,34 +5,52 @@ import { v4 as newId } from "uuid";
 const router = express.Router();
 
 router.get("/foods", (req, res) => {
-    const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
-    res.json(foods);
+  const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
+  res.json(foods);
 });
 
 router.get("/foods/:id", (req, res) => {
-    const foodId = req.params.id;
-    const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
-    const foodById = foods.find(f => f.id === foodId);
-    res.json(foodById);
+  const foodId = req.params.id;
+  const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
+  const foodById = foods.find((f) => f.id === foodId);
+  res.json(foodById);
 });
 
-router.get("/foods/:id/comment", (req, res) => {
-    const { name, date, comment } = req.body;
-    const foodId = req.params.id;
-    const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
-    foods.forEach(food => {
-        food.comments.push({
-            name: name,
-            date: date,
-            comment: comment
-        })
-    })
+router.post("/foods/:id/comment", (req, res) => {
+  const { name, comment } = req.body;
+  const foodId = req.params.id;
+  const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
+  foods.forEach((food) => {
+    if (food.id === foodId) {
+      food.comments.push({
+        id: newId(),
+        name: name,
+        date: Date.now(),
+        comment: comment,
+      });
+    }
+  });
 
-    fs.writeFileSync("./Data/foods.json", JSON.stringify(foods));
-    
-    const foodById = foods.find(f => f.id === foodId);
-    console.log("Food added: ", foodById);
-    res.json(foodById);
+  fs.writeFileSync("./Data/foods.json", JSON.stringify(foods));
+
+  const foodById = foods.find((f) => f.id === foodId);
+  res.json(foodById);
+});
+
+router.delete("/foods/:id/comments/:commentId", (req, res) => {
+  const commentId = req.params.commentId;
+  const foodId = req.params.id;
+  const foods = JSON.parse(fs.readFileSync("./Data/foods.json"));
+  foods.forEach((food) => {
+    if (food.id === foodId) {
+      food.comments = food.comments.filter((c) => c.id !== commentId);
+    }
+  });
+
+  fs.writeFileSync("./Data/foods.json", JSON.stringify(foods));
+
+  const foodById = foods.find((f) => f.id === foodId);
+  res.json(foodById.comments);
 });
 
 export default router;
